a2568775dbd3b15d4c3e12f55373594ceb34e6e4
Tim Kelty <tim@fusionary.com>
2018-08-17 10:22:38 -0400

    Better CraftQL integration:
    - Bring back Category (supported by CraftQL now)
    - Add resolved `text` field
    - Add `link` field
      - Accepts `text` arg override
      - Ultimately, this should also include an `attributes` object argument to pass/override any html attrs
    - Remove allowCustomText and allowTarget, as they didn't seem relevant to query for

--- src/utilities/CraftQLListener.php
+++ src/utilities/CraftQLListener.php
@@ -2,10 +2,12 @@
 
 namespace typedlinkfield\utilities;
 
+use craft\elements\Category;
 use craft\elements\Asset;
 use craft\elements\Entry;
 use markhuot\CraftQL\Builders\Schema;
 use markhuot\CraftQL\Events\GetFieldSchema;
+use GraphQL\Type\Definition\InputObjectType;
 use typedlinkfield\fields\LinkField;
 use typedlinkfield\models\ElementLinkType;
 use typedlinkfield\models\Link;
@@ -20,9 +22,7 @@ class CraftQLListener {
    * @var array
    */
   static $QL_TYPES = [
-    // Category seems to be deprecated and GlobalSet has no interface we can refer to:
-    // Category::class  => \markhuot\CraftQL\Types\CategoryInterface::class,
-    // GlobalSet::class => \markhuot\CraftQL\Types\GlobalsSet::class,
+    Category::class  => \markhuot\CraftQL\Types\CategoryInterface::class,
     Asset::class     => \markhuot\CraftQL\Types\VolumeInterface::class,
     Entry::class     => \markhuot\CraftQL\Types\EntryInterface::class,
   ];
@@ -67,11 +67,16 @@ class CraftQLListener {
       $types[] = $linkName;
     }
 
-    $object->addBooleanField('allowCustomText');
-    $object->addBooleanField('allowTarget');
+    $link = $object->addStringField('link');
+    $link->addStringArgument('text');
+    $link->resolve(function($link, $args) {
+        return $link instanceof Link ? (string) $link->getLink($args['text']) : '';
+    });
+
     $object->addStringField('customText');
     $object->addStringField('defaultText');
     $object->addStringField('target');
+    $object->addStringField('text');
     $object->addEnumField('type')->values($types);
     $object->addStringField('url')->resolve(function($link) {
       return $link instanceof Link ? $link->getUrl() : '';
