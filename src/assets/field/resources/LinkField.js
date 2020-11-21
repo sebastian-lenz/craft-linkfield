c433d28afa5b7acc112cec534fe5136c7c1ae1f4
Sebastian Lenz <github@sebastian-lenz.de>
2020-11-21 22:14:55 +0100

    Use a resize observer to improve small field styling, see #64

--- src/assets/field/resources/LinkField.js
+++ src/assets/field/resources/LinkField.js
@@ -22,6 +22,8 @@
 		$optionsHolder: null,
 		$settingsHolder: null,
 		$typeSelect: null,
+    resizeObserver: null,
+    resizeClass: '',
 		type: null,
 
 		init: function(id) {
@@ -36,13 +38,55 @@
       this.$field.find('input[type=hidden]').each(createSiteHelper);
 
 			this.addListener(this.$typeSelect, 'change', 'onChangeType');
+      this.resizeObserver = this.createResizeObserver();
 		},
+
+    createResizeObserver: function() {
+		  var that = this;
+      function handler(entries) {
+        for (var index = 0; index < entries.length; index++) {
+          var entry = entries[index];
+          if (entry.contentBoxSize && entry.contentBoxSize[0]) {
+            that.onResize(entry.contentBoxSize[0].inlineSize);
+          } else if (entry.contentBoxSize) {
+            that.onResize(entry.contentBoxSize.inlineSize);
+          } else {
+            that.onResize(entry.contentRect.width);
+          }
+        }
+      }
+
+      try {
+        var resizeObserver = new ResizeObserver(handler);
+        resizeObserver.observe(this.$field[0]);
+        return resizeObserver;
+      } catch (error) {
+        console.error(error);
+      }
+
+      return null;
+    },
 
 		onChangeType: function(e) {
 			this.type = this.$typeSelect.val();
       this.$settingsHolder.toggleClass('hidden', this.type === '' || this.type === 'empty');
 			this.$options.addClass('hidden');
 			this.$options.filter('.' + this.type).removeClass('hidden');
-		}
+		},
+
+		onResize: function(width) {
+		  var resizeClass = '';
+		  if (width < 300) {
+        resizeClass = 'xs';
+      } else if (width < 500) {
+        resizeClass = 'sm';
+      }
+
+      if (this.resizeClass !== resizeClass) {
+        this.$field.removeClass(this.resizeClass);
+        this.$field.addClass(resizeClass);
+        this.resizeClass = resizeClass;
+      }
+    }
 	});
 })(jQuery);
